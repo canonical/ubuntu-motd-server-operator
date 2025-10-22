9429f966a54b9b9ef38f321dcb0ea4208026843b
Sébastien Georget <sebastien.georget@canonical.com>
2025-10-22 14:02:52 +0200

    Feat/terraform product (#7)

---
+++ terraform-product/main.tf
@@ -0,0 +1,39 @@
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+module "ubuntu_motd_server" {
+  source      = "../terraform"
+  app_name    = var.ubuntu_motd_server.app_name
+  channel     = var.ubuntu_motd_server.channel
+  config      = var.ubuntu_motd_server.config
+  model       = var.model
+  constraints = var.ubuntu_motd_server.constraints
+  revision    = var.ubuntu_motd_server.revision
+  base        = var.ubuntu_motd_server.base
+  units       = var.ubuntu_motd_server.units
+}
+
+module "traefik_k8s" {
+  source      = "git::ssh://git@github.com/canonical/traefik-k8s-operator//terraform?depth=1&ref=rev254"
+  app_name    = var.traefik_k8s.app_name
+  channel     = var.traefik_k8s.channel
+  config      = var.traefik_k8s.config
+  constraints = var.traefik_k8s.constraints
+  model       = var.model
+  revision    = var.traefik_k8s.revision
+  units       = var.traefik_k8s.units
+}
+
+resource "juju_integration" "motd_traefik" {
+  model = var.model
+
+  application {
+    name     = module.ubuntu_motd_server.app_name
+    endpoint = module.ubuntu_motd_server.endpoints.ingress
+  }
+
+  application {
+    name     = module.traefik_k8s.app_name
+    endpoint = module.traefik_k8s.endpoints.ingress
+  }
+}
